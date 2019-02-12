6ebff2ea7589566803228a3da567a01d6364ceb1
tanjoin <tanjoin@users.noreply.github.com>
2019-02-12 14:12:48 +0900

    3時間おきにチェック

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const CronJob = require('cron').CronJob;
 const exec = require('child_process').exec;
 
-new CronJob('00 */2 * * *', async () => {
+new CronJob('00 */3 * * *', async () => {
   console.log('cron start!');
   exec('./run.sh', (err, stdout, stderr) => {
     if (err) {
