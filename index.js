e4a9610561b05c1901a60d3ea7ea967827f2b4f7
tanjoin <tanjoin@users.noreply.github.com>
2026-02-06 12:55:19 +0900

    class 化

--- index.js
+++ index.js
@@ -1,66 +1,87 @@
 const CronJob = require('cron').CronJob;
 const spawn = require('child_process').spawn;
 
-function spawnPromise(command, args, options) {
-  return new Promise((resolve, reject) => {
-    const child = spawn(command, args, options);
-    child.on('close', (code) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(new Error(`Process exited with code: ${code}`));
-      }
+class Radipy {
+  constructor({ timezone = 'Asia/Tokyo', shell = '/bin/zsh' } = {}) {
+    this.timezone = timezone;
+    this.shell = shell;
+  }
+
+  spawn(command, args, options) {
+    return new Promise((resolve, reject) => {
+      const child = spawn(command, args, options);
+      child.on('close', (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(new Error(`Process exited with code: ${code}`));
+        }
+      });
+      child.on('error', reject);
     });
-  });
-};
+  }
 
-async function cron() {
-  new CronJob('30 7 * * *', async () => {
-    console.log('get list!');
-    // Use explicit shell interpreter to avoid shell:true
-    await spawnPromise('/bin/zsh', ['list.sh'], { stdio: 'inherit' });
-  }, null, true, 'Asia/Tokyo');
+  async organize() {
+    await this.spawn('pipenv', ['run', 'org'], {
+      stdio: 'inherit',
+      env: { ...process.env, PIPENV_DONT_LOAD_ENV: 1 },
+    });
+  }
 
-  new CronJob('30 8 * * *', async () => {
-    console.log('cron start!' + new Date().toLocaleString('ja-JP'));
-    // run.sh has a shebang; call directly without shell
-    await spawnPromise('/bin/zsh', ['run.sh'], { stdio: 'inherit' });
-  }, null, true, 'Asia/Tokyo');
+  async list(dayBack) {
+    const args = ['list.sh'];
+    if (dayBack !== undefined) args.push(dayBack);
+    await this.spawn(this.shell, args, { stdio: 'inherit' });
+  }
 
-  new CronJob('0 6 * * *', async () => {
-    console.log('organize files!');
-    // Call pipenv directly with args; no shell needed
-    await spawnPromise('pipenv', ['run', 'org'], { stdio: 'inherit', env: {
-      ...process.env,
-      PIPENV_DONT_LOAD_ENV: 1,
-    } });
-  }, null, true, 'Asia/Tokyo');
-}
+  async run() {
+    await this.spawn(this.shell, ['run.sh'], { stdio: 'inherit' });
+  }
 
-async function main() {
-  await spawnPromise('pipenv', ['run', 'org'], { stdio: 'inherit', env: {
-    ...process.env,
-    PIPENV_DONT_LOAD_ENV: 1,
-  } });
-  const dayBack = (() => {
-    const dIndex = process.argv.indexOf('-d');
-    if (dIndex !== -1 && process.argv.length > dIndex + 1) {
-      return process.argv[dIndex + 1];
+  startCron() {
+    new CronJob('30 7 * * *', async () => {
+      console.log('get list!');
+      await this.list();
+    }, null, true, this.timezone);
+
+    new CronJob('30 8 * * *', async () => {
+      console.log('cron start!' + new Date().toLocaleString('ja-JP'));
+      await this.run();
+    }, null, true, this.timezone);
+
+    new CronJob('0 6 * * *', async () => {
+      console.log('organize files!');
+      await this.organize();
+    }, null, true, this.timezone);
+  }
+
+  static parseDayBack(argv) {
+    const dIndex = argv.indexOf('-d');
+    if (dIndex !== -1 && argv.length > dIndex + 1) {
+      return argv[dIndex + 1];
     }
-    const dbIndex = process.argv.indexOf('--day-back');
-    if (dbIndex !== -1 && process.argv.length > dbIndex + 1) {
-      return process.argv[dbIndex + 1];
+    const dbIndex = argv.indexOf('--day-back');
+    if (dbIndex !== -1 && argv.length > dbIndex + 1) {
+      return argv[dbIndex + 1];
     }
     return undefined;
-  })();
-  await spawnPromise('/bin/zsh', ['list.sh', dayBack], { stdio: 'inherit' });
-  await spawnPromise('/bin/zsh', ['run.sh'], { stdio: 'inherit' });
+  }
+
+  async runOnce(argv = process.argv) {
+    await this.organize();
+    const dayBack = Radipy.parseDayBack(argv);
+    await this.list(dayBack);
+    await this.run();
+  }
 }
 
+module.exports = { Radipy };
+
 if (require.main === module) (async () => {
+  const radipy = new Radipy();
   if (process.argv.includes('--cron')) {
-    await cron();
+    radipy.startCron();
   } else {
-    await main();
+    await radipy.runOnce(process.argv);
   }
 })();
