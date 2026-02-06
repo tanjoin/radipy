168a9367d1a110d5635fa4449fe37a4504258752
tanjoin <tanjoin@users.noreply.github.com>
2026-02-06 13:03:47 +0900

    整形

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const CronJob = require('cron').CronJob;
-const spawn = require('child_process').spawn;
+const CronJob = require("cron").CronJob;
+const spawn = require("child_process").spawn;
 
 class Radipy {
-  constructor({ timezone = 'Asia/Tokyo', shell = '/bin/zsh' } = {}) {
+  constructor({ timezone = "Asia/Tokyo", shell = "/bin/zsh" } = {}) {
     this.timezone = timezone;
     this.shell = shell;
   }
@@ -10,59 +10,64 @@ class Radipy {
   spawn(command, args, options) {
     return new Promise((resolve, reject) => {
       const child = spawn(command, args, options);
-      child.on('close', (code) => {
+      child.on("close", (code) => {
         if (code === 0) {
           resolve();
         } else {
           reject(new Error(`Process exited with code: ${code}`));
         }
       });
-      child.on('error', reject);
+      child.on("error", reject);
     });
   }
 
   async organize() {
-    await this.spawn('pipenv', ['run', 'org'], {
-      stdio: 'inherit',
+    await this.spawn("pipenv", ["run", "org"], {
+      stdio: "inherit",
       env: { ...process.env, PIPENV_DONT_LOAD_ENV: 1 },
     });
   }
 
   async list(dayBack) {
-    const args = ['list.sh'];
+    const args = ["list.sh"];
     if (dayBack !== undefined) args.push(dayBack);
-    await this.spawn(this.shell, args, { stdio: 'inherit' });
+    await this.spawn(this.shell, args, { stdio: "inherit" });
   }
 
   async run() {
-    await this.spawn(this.shell, ['run.sh'], { stdio: 'inherit' });
+    await this.spawn(this.shell, ["run.sh"], { stdio: "inherit" });
   }
 
   startCron() {
-    new CronJob('30 7 * * *', async () => {
-      console.log('get list!');
+    new CronJob("30 7 * * *", async () => {
+      console.log("get list!");
       await this.list();
     }, null, true, this.timezone);
 
-    new CronJob('30 8 * * *', async () => {
-      console.log('cron start!' + new Date().toLocaleString('ja-JP'));
+    new CronJob("30 8 * * *", async () => {
+      console.log("cron start!" + new Date().toLocaleString("ja-JP"));
       await this.run();
     }, null, true, this.timezone);
 
-    new CronJob('0 6 * * *', async () => {
-      console.log('organize files!');
+    new CronJob("0 6 * * *", async () => {
+      console.log("organize files!");
       await this.organize();
     }, null, true, this.timezone);
   }
 
   static parseDayBack(argv) {
-    const dIndex = argv.indexOf('-d');
-    if (dIndex !== -1 && argv.length > dIndex + 1) {
-      return argv[dIndex + 1];
-    }
-    const dbIndex = argv.indexOf('--day-back');
-    if (dbIndex !== -1 && argv.length > dbIndex + 1) {
-      return argv[dbIndex + 1];
+    // support: -d 1, -d=1, --day-back 1, --day-back=1
+    const aliases = new Set(["-d", "--day-back"]);
+    for (let i = 0; i < argv.length; i++) {
+      const token = argv[i];
+      const eq = token.indexOf("=");
+      if (eq !== -1) {
+        const key = token.slice(0, eq);
+        const val = token.slice(eq + 1);
+        if (aliases.has(key)) return val;
+      } else if (aliases.has(token) && i + 1 < argv.length) {
+        return argv[i + 1];
+      }
     }
     return undefined;
   }
@@ -79,7 +84,7 @@ module.exports = { Radipy };
 
 if (require.main === module) (async () => {
   const radipy = new Radipy();
-  if (process.argv.includes('--cron')) {
+  if (process.argv.includes("--cron")) {
     radipy.startCron();
   } else {
     await radipy.runOnce(process.argv);
